1c54d4f71751c6f6ad6af718d3a1d210a2d0705c
zikner <mitchyjeffy@gmail.com>
2018-03-15 14:56:08 +0300

    Initial Commit

---
+++ grabber/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import ConnectionData
+
+# Register your models here.
+
+admin.site.register(ConnectionData)
---
+++ grabber/trash.py
@@ -0,0 +1,17 @@
+# class Test:
+
+# 	def test_method(self):
+# 		lst = []
+# 		def nested():
+
+# 			lst.append("jeffy")
+
+# 		nested()
+		
+# 	print(lst)
+
+
+# obj = Test()
+# obj.test_method()
+
+print("/root/name/address/".lstrip('/root'))
